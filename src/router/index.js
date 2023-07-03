2806b5492c46890469f686310651ef47bc7a3745
Ken Ballon <bkenn003@gmail.com>
2023-07-03 17:35:39 +0800

    Second commit

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,11 @@ const router = createRouter({
       name: "home",
       component: HomeView,
     },
+    {
+      path: "/add",
+      name: "AddTask",
+      component: () => import("../views/AddNewTask.vue"),
+    },
   ],
 });
 
---
+++ src/views/AddNewTask.vue
@@ -0,0 +1,30 @@
+<template >
+    <div>
+        <form @submit.prevent="handleSubmit">
+            <div class="form-group">
+                <label for="task-heading">Task:</label>
+                <input type="text" name="task-heading" id="task-heading" v-model="data.title" required>
+            </div>
+            <div class="form-group">
+                <label for="task-details">Details:</label>
+                <textarea name="task-details" id="task-details" v-model="data.details"></textarea>
+            </div>
+            <div class="form-group">
+                <button>Add Task</button>
+            </div>
+        </form>
+    </div>
+</template>
+<script setup>
+import { ref } from 'vue';
+
+const data = ref({
+    title: '',
+    details: ''
+})
+
+const handleSubmit = () => {
+    console.log(`Task: ${data.value.title} \n Details: ${data.value.details}`);
+}
+</script>
+<style></style>
