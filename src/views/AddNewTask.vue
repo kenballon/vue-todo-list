c96f27772aab881f5932f05d978b6e6f6a8cd5b9
Ken Ballon <bkenn003@gmail.com>
2023-07-03 19:27:16 +0800

    css changes

--- src/views/AddNewTask.vue
+++ src/views/AddNewTask.vue
@@ -1,5 +1,6 @@
 <template >
-    <div>
+    <div class="form-wrapper">
+        <h1>Add New Task</h1>
         <form @submit.prevent="handleSubmit">
             <div class="form-group">
                 <label for="task-heading">Task:</label>
@@ -9,8 +10,8 @@
                 <label for="task-details">Details:</label>
                 <textarea name="task-details" id="task-details" v-model="data.details"></textarea>
             </div>
-            <div class="form-group">
-                <button>Add Task</button>
+            <div class="form-group d-flex align-items-center">
+                <button class="submit-btn">Add Task</button>
             </div>
         </form>
     </div>
@@ -27,4 +28,54 @@ const handleSubmit = () => {
     console.log(`Task: ${data.value.title} \n Details: ${data.value.details}`);
 }
 </script>
-<style></style>
+<style>
+.form-wrapper {
+    max-width: 700px;
+    margin-inline: auto;
+    margin-block: 4rem;
+    background-color: white;
+    padding: 3rem;
+    border-radius: 8px;
+    box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+}
+
+.form-wrapper .form-group {
+    margin-bottom: 1.5rem;
+}
+
+.form-wrapper .form-group label,
+.form-wrapper .form-group input,
+.form-wrapper .form-group textarea {
+    display: block;
+    width: 100%;
+}
+
+.form-wrapper .form-group {
+
+    & input,
+    & textarea {
+        padding: .5rem 1rem;
+        font-family: 'Satoshi', sans-serif;
+    }
+
+    .submit-btn {
+        margin-left: auto;
+        font-family: 'Satoshi', sans-serif;
+        border: 1px solid transparent;
+        padding: .5rem 1rem;
+        background-color: #00591b;
+        color: white;
+        transition: color .3s ease;
+        cursor: pointer;
+
+        &:hover {
+            background-color: #00a331;
+        }
+    }
+
+}
+
+.form-wrapper .form-group textarea {
+    min-height: 250px;
+}
+</style>
